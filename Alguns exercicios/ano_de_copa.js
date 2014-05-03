d9fc43d8279572ce53fd77685294baf99a0f81df
Vinicius Baptista <vbaptista7@gmail.com>
2014-05-02 22:56:27 -0300

    Update ano_de_copa.js

--- Alguns exercicios/ano_de_copa.js
+++ Alguns exercicios/ano_de_copa.js
@@ -3,12 +3,12 @@ function mostra(frase) {
 };
 
 var anoComoTexto = prompt("Informe o ano inicial");
-var anoDeCopas = parseInt(anoComoTexto);
+var anoDeCopa = parseInt(anoComoTexto);
 var anoLimite = prompt("Qual é o ano limite?");
 
-while(anoDeCopas <= anoLimite) {
-  mostra(anoDeCopas + " tem Copa!");
-  anoDeCopas = anoDeCopas + 4;
+while(anoDeCopa <= anoLimite) {
+  mostra(anoDeCopa + " tem Copa!");
+  anoDeCopa = anoDeCopa + 4;
 } 
 
 mostra("Até que enfim, esses foram os anos de copa até " + anoLimite);
